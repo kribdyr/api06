c0915aa7099875d27146cd8457c0bb4f5cbe6f8c
kribdyr <71062475+kribdyr@users.noreply.github.com>
2025-11-06 10:12:02 +0100

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using api06.Models;
+using api06.Repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api06.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository _repository;
+
+        public BookController(BookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get() => _repository.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var book = _repository.GetById(id);
+            return book is null ? NotFound() : Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Post(Book book)
+        {
+            var addedBook = _repository.Add(book);
+            return CreatedAtAction(nameof(Get), new { id = addedBook.Id }, addedBook);
+        }
+    }
+}
---
+++ Repo/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using api06.Models;
+
+namespace api06.Repo
+{
+    public class BookRepository
+    {
+        private readonly List<Book> _books = new();
+
+        public IEnumerable<Book> GetAll() => _books;
+
+        public Book? GetById(int id) => _books.FirstOrDefault(b => b.Id == id);
+
+        public Book Add(Book book)
+        {
+            book.Id = _books.Count + 1;
+            _books.Add(book);
+            return book;
+        }
+    }
+}
